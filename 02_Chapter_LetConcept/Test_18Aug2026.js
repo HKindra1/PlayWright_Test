82059be9e80c326aa84ded08b7bb24663433e6b2
Vandana <vandana@example.com>
2026-08-22 15:56:35 +0530

    Restore 18-Aug test file

---
+++ 02_Chapter_LetConcept/Test_18Aug2026.js
@@ -0,0 +1,49 @@
+console.log(typeof "PlayWright");
+
+console.log(typeof 42);
+
+console.log(typeof true);
+
+console.log(typeof undefined);
+
+console.log(typeof null);
+
+console.log(typeof NaN);
+
+typeof 42;
+
+let a;
+
+consol.log(a);
+
+let score = 90; 
+score = "pass"; 
+console.log(typeof score);
+
+    console.log(a);
+    var a=10;
+
+
+console.log(a);
+let a=10;
+
+console.log(a);
+Const a=10;
+
+greet();
+function greet() {
+  console.log("Hi");
+}
+
+sayHi();
+const sayHi = function() {
+  console.log("Hi");
+};
+
+{
+  console.log(testName);
+  let testName = "Login Test";
+}
+
+let count = count + 1;
+console.log(count);
